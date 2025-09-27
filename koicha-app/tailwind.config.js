2883d3719abade114f11f022fd9db0d31a0aa98a
klee2024 <kaya.m.lee7@gmail.com>
2025-09-26 20:31:11 -0400

    set up basic tailwind, TODO: add utilities for brand

---
+++ koicha-app/tailwind.config.js
@@ -0,0 +1,16 @@
+export const content = ["./src/**/*.{html,ts}"];
+export const theme = {
+  extend: {
+    colors: {
+      green: {
+        500: "var(--koicha-green-500)",
+        700: "var(--koicha-green-700)",
+      },
+      rust: {
+        500: "var(--koicha-rust-500)",
+        700: "var(--koicha-rust-700)",
+      },
+    },
+  },
+  fontFamily: {},
+};
