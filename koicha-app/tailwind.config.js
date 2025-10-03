b1e92f022d08f100414346c923cf630b8425bb0e
klee2024 <kaya.m.lee7@gmail.com>
2025-10-02 20:54:16 -0400

    integrate tailwind successfully with tokens'

--- koicha-app/tailwind.config.js
+++ koicha-app/tailwind.config.js
@@ -1,16 +1,19 @@
-export const content = ["./src/**/*.{html,ts}"];
-export const theme = {
-  extend: {
-    colors: {
-      green: {
-        500: "var(--koicha-green-500)",
-        700: "var(--koicha-green-700)",
-      },
-      rust: {
-        500: "var(--koicha-rust-500)",
-        700: "var(--koicha-rust-700)",
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          500: "var(--koicha-green-500)",
+          700: "var(--koicha-green-700)",
+        },
+        rust: {
+          500: "var(--koicha-rust-500)",
+          700: "var(--koicha-rust-700)",
+        },
       },
     },
+    fontFamily: {},
   },
-  fontFamily: {},
+  plugins: [],
 };
