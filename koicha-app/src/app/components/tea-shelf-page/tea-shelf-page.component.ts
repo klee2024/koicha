6e486c9056fd36842452cf0a934e5b3b59c9d26f
klee2024 <kaya.m.lee7@gmail.com>
2025-10-06 19:26:45 -0400

    set up child pages for tea shelf

--- koicha-app/src/app/components/tea-shelf-page/tea-shelf-page.component.ts
+++ koicha-app/src/app/components/tea-shelf-page/tea-shelf-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-tea-shelf-page',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './tea-shelf-page.component.html',
-  styleUrl: './tea-shelf-page.component.css'
+  styleUrl: './tea-shelf-page.component.css',
 })
-export class TeaShelfPageComponent {
-
-}
+export class TeaShelfPageComponent {}
