0cdb6d38a7ab9cc7607db927c874a857a3e52363
klee2024 <kaya.m.lee7@gmail.com>
2025-11-12 20:04:59 -0500

    create component for expanded review details card

---
+++ koicha-app/src/app/components/review-details/review-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ReviewCard } from '../utility/product-card/productCardData';
+import { ProductCardContentComponent } from '../utility/product-card-content/product-card-content.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-review-details',
+  imports: [ProductCardContentComponent, CommonModule],
+  templateUrl: './review-details.component.html',
+  styleUrl: './review-details.component.css',
+})
+export class ReviewDetailsComponent {
+  @Input() selectedReviewCard: ReviewCard | undefined;
+
+  @Output() closed = new EventEmitter<boolean>();
+}
