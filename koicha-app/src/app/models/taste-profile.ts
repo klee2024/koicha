6775eac82227ba7e84ddd6538aa4c4c5999315aa
klee2024 <kaya.m.lee7@gmail.com>
2025-11-15 11:06:25 -0500

    update TasteProfile model to include details

--- koicha-app/src/app/models/taste-profile.ts
+++ koicha-app/src/app/models/taste-profile.ts
@@ -1,6 +1,9 @@
 import { SubFlavorCharacteristic } from './sub-flavor-characteristics';
+import { TasteProfileDetails } from './taste-profile-details';
 
 export interface TasteProfile {
+  id: string;
   mainCharacterstics: Record<string, number> | undefined;
   subCharacteristics: Record<string, SubFlavorCharacteristic[]> | undefined;
+  tasteProfileDetails: TasteProfileDetails;
 }
