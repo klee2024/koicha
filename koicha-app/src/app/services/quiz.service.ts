d7e13b517ff3b72da72cc4e513c7eb7b2cda270e
klee2024 <kaya.m.lee7@gmail.com>
2025-12-06 18:00:25 -0500

    update baseUrl and create environment file

---
+++ koicha-app/src/app/services/quiz.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { delay, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Quiz } from '../models/Quiz';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuizService {
+  private readonly baseUrl = 'http://127.0.0.1:8000/api'; // e.g. 'http://localhost:8000/api/quiz'
+
+  constructor(private http: HttpClient) {}
+
+  getQuiz(quiz_slug: string) {
+    return this.http.get<Quiz>(`${this.baseUrl}/quiz/${quiz_slug}/latest/`);
+  }
+
+  submitQuiz(
+    userId: string,
+    quizResults: { answers: (string | null)[] }
+  ) {
+    return of({ success: true }).pipe(delay(100));
+  }
+}
